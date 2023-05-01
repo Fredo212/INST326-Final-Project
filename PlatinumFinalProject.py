a2001c5cb806a5b10188c342a184970e99ad1657
akipar <akipar@terpmail.umd.edu>
2023-05-01 10:10:56 -0400

    wrote the recommend function

--- PlatinumFinalProject.py
+++ PlatinumFinalProject.py
@@ -1,4 +1,6 @@
 #This function's goal is to take the user input of budget and optional input of "important" which can be either food, money, or convinience.
+import math
+import re
 
 class Travel:
   '''Sets attributes
@@ -7,7 +9,7 @@ class Travel:
       budget (int): Brief budget to go to that place
       important (str, optional): Important factor of travel that is originally set to "price." User may set this to price, food or convenience.
   '''
-  def __init__(self, destination, budget, important = price):
+  def __init__(self, destination, budget, important = "price"):
  #Aki will finish this part
     self.destination = destination
     self.budget = budget
@@ -29,23 +31,22 @@ class Travel:
         self.destination = line[0]
         self.budget = line[1]
         self.important = line[2]
-        
-  def recommend(self, budget, important = price):
-    '''Finds the recommended destination from the file
+   
+  def makeList(self, budget, important = "price"):
+    #change it to Pandas sorting
+    '''Finds the recommended destination, add them to list
     Args:
      budget (int): Brief budget to go to that place
      important (str, optional): Important factor of travel that is originally set to "price." User may s
     '''
     recList = []
-    for Readfile(filepath):
-      try math.isclose(budget, self.budget, rel_tol = 300):
-        if True:
-          recList.append(destination)
-         else:
-          continue
+    for n in self.budget:
+      check = (math.isclose(budget, self.budget, rel_tol = 300)):
+      if check == True:
+        recList.append(destination)
     return recList
           
-      
+
   
   def __repr__(self):
  #returns the final statement of where the user could go
@@ -59,3 +60,5 @@ class Travel:
       #raise Exception(f"An error occured
       # while generating the string representation of the Travel object: {e}")
     return f"Travel(destination ='{self.destination}', price={self.price})"
+  
+def main(budget, important = price):
