3a3ed33df0c7c6171085dc881e254cb9a1736ba2
Younju Kim <younju77@terpmail.umd.edu>
2023-05-03 08:12:38 -0400

    Done

--- PlatinumFinalProject.py
+++ PlatinumFinalProject.py
@@ -41,22 +41,48 @@ class Travel:
     recommended = BudgetRec[BudgetRec["important"] == "price"]
     return recommended
           
-
+  
+  # If you want to change this code for read file, you can use this code. (Younju Kim)
+  def from_file(self,filepath):
+    """
+    reads a text file and creates a list of Travel objects based on data in the text file.
+    
+    Args:
+      filepath (string): file that will be passed in this function to be read
+      
+    Returns:
+      A list of Travel objects
+    """
+    travels = []
+    with open(filepath, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.split()
+            destination = line[0]
+            budget = int(line[1])
+            important = line[2]
+            travel = Travel(destination, budget, important)
+            travels.append(travel)
+    return travels
   
   def __repr__(self):
- #returns the final statement of where the user could go
- #Younju will do this part
+  
     """
     Returns a string representation of the Travel object.
     """
-    #try:
-      #return f"Travel(destination='{self.destination}',price = {self.price})
-    #except Exception as e:
-      #raise Exception(f"An error occured
-      # while generating the string representation of the Travel object: {e}")
-    return f"Travel(destination ='{self.destination}', price={self.price})"
-
-
+    return f"Travel(destination ='{self.destination}', budget={self.budget}, important = '{self.important}')"
+  
+  def __str__(self):
+    """
+    Returns a human-readable string representation of the Travel object.
+    """
+    return f"{self.destination} ({self.budget} budget, important: {self.important})"
+  
+  def __lt__(self,other):
+    """
+    Defines a less-than comparison between two Travel objects.
+    """
+    return self.budget < other.budget
+    
 class Vacation(Travel): 
   def __init__(self): 
     super().__init__("fiji", 20000, "price")
