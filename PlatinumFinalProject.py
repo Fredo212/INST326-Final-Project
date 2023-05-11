737f53183560291d9b6d5bdd9d2a04a1d395e5b2
Fredo212 <91163956+Fredo212@users.noreply.github.com>
2023-05-11 18:18:22 -0400

    Update PlatinumFinalProject.py

--- PlatinumFinalProject.py
+++ PlatinumFinalProject.py
@@ -9,25 +9,19 @@ class Travel:
     Attributes:
       destination (str): The destination for the travel.
       budget (int): The budget for the travel
-      important (str): The important factor of the travel. Default is "price".
-      
-    Methods:
-      recommend(df, budget, important="price"):
-        Finds the recommended destination from the provided dataframe based on the given budget and important factor.
-        
-      __repr__():
-        Returns a string representation of the Travel object.
-      
-      __str__():
-        Returns a human-readable string representation of the Travel object.
-      
-      __lt__(other):
-        Defines a less-than comparison between two Travel objects based on their budgets.
+      important (str, optional): The important factor of the travel. Default is "price".
     """
     def __init__(self, destination, budget, important="price"):
-        self.destination = destination
-        self.budget = budget
-        self.important = important
+      """_summary_
+
+      Args:
+          destination (str): The destination for the travel.
+          budget (str): The budget for the travel
+          important (str, optional): The important factor of the travel. Defaults to "price".
+      """
+      self.destination = destination
+      self.budget = budget
+      self.important = important
 
     def recommend(self, df, budget, important="price"):
         """
@@ -41,10 +35,8 @@ class Travel:
         Returns:
           recommended (pd.DataFrame): The recommended destinations based on the input parameters.
         """
-        
         BudgetRec = df[(df["Budget"] > budget - 300) & (df["Budget"] < budget + 300)]
         recommended = BudgetRec[BudgetRec["Important (optional)"].str.contains(important, case=False)]
-        
         return recommended
 
     def __repr__(self):
@@ -66,37 +58,9 @@ class Travel:
         return self.budget < other.budget
 
 class Vacation(Travel):
-    """
-    A child class representing a Vacation, using all features of its parent, Travel.
-    
-    Attributes:
-      destination (str): The destination for the vacation.
-      budget (int): The budget for the vacation
-      important (str): The important factor of the vacation. Default is "luxury".
-      
-    Methods:
-      Overriden:
-      recommend(df, budget, important="price"):
-        Finds the recommended destination from the provided dataframe based on the given budget and important factor.
-        
-      all other methods from the parent class (Travel)
-    """
-    def __init__(self, destination, budget, important="luxury"):
-        super().__init__(self.destination, self.budget, self.important)
-
-    """
-        Finds the recommended destination from the provided dataframe based on the given budget and important factor.
-        
-        Args:
-          df (pd.DataFrame): The dataframe containing information about vacation destinations.
-          budget (int): The brief budget to go to that place.
-          important (str, optional): The important factor of vacation. Default is "luxury".
-          
-        Returns:
-          recommended (pd.DataFrame): The recommended vacations based on the input parameters.
-        """
-    def recommend(self, df, budget, important="luxury"):
-        return super().recommend(df, budget, important)
+    def __init__(self):
+        super().__init__("Fiji", 20000, "price")
+        print("This vacation is to: Fiji, it costs: 20000")
 
 
 def read_file(filepath):
@@ -134,24 +98,6 @@ def from_file(filepath):
             travels.append(travel)
     return travels
     
-def parse_args(arglist):
-    """
-    Parses the command-line arguments.
-    
-    Args:
-      arglist (list): List of command-line arguments.
-      
-    Returns:
-
-    """
-    parser = ArgumentParser()
-    parser.add_argument("filepath", help="path to CSV File")
-    parser.add_argument("destination", help="name of destination")
-    parser.add_argument("budget", type=int, help="budget for trip")
-    parser.add_argument("-i", "--important", default="price",
-                        help="originally set to price. User may set this to price, food or convenience.")
-    args = parser.parse_args(arglist)
-    return args
 
 def recommend_destinations(df, destination, budget, important_factor):
     """
@@ -169,9 +115,8 @@ def recommend_destinations(df, destination, budget, important_factor):
     travel_obj = Travel(destination, budget, important_factor)
     recommended_destinations = travel_obj.recommend(df, budget, important_factor)
     print(f"\nRecommended destinations for your budget and important factor ({important_factor}):")
-    # Use list comprehension to generate the recommended destinations string
-    destinations_str = '\n'.join([f"- {row['Destination']} ({row['Budget']} budget, important: {row['Important (optional)']})" for _, row in recommended_destinations.iterrows()])
-    print(destinations_str)
+    for idx, row in recommended_destinations.iterrows():
+        print(f"- {row['Destination']} ({row['Budget']} budget, important: {row['Important (optional)']})")
     return recommended_destinations
 
 
@@ -182,21 +127,19 @@ def display_discounted_price(recommended_destinations):
     Args:
       recommended_destinations (pd.DataFrame): The recommended destinations.
       
-    Returns:
-      None
     """
     print("Updating data...")
     discounted_destination = recommended_destinations.sample(n=1).iloc[0]
     original_budget = discounted_destination['Budget']
-  
+    discounted_budget = int(original_budget * 0.8)  # Apply 20% discount
 
     print(f"\nUpdated price for {discounted_destination['Destination']}:")
-    print(f"- {discounted_destination['Destination']} ({int(original_budget * 0.8)} budget, important: {discounted_destination['Important (optional)']})")
+    print(f"- {discounted_destination['Destination']} ({discounted_budget} budget, important: {discounted_destination['Important (optional)']})")
 
     print("\nRecommending destinations after the update:")
     for idx, row in recommended_destinations.iterrows():
         if row['Destination'] == discounted_destination['Destination']:
-            print(f"- {row['Destination']} ({int(original_budget * 0.8)} budget, important: {row['Important (optional)']})")
+            print(f"- {row['Destination']} ({discounted_budget} budget, important: {row['Important (optional)']})")
         else:
             print(f"- {row['Destination']} ({row['Budget']} budget, important: {row['Important (optional)']})")
 
@@ -231,8 +174,6 @@ def main(filepath, destination, budget, important_factor="price"):
       budget (int): The budget for the trip.
       important_factor (str, optional): The important factor for the trip. Default is "price".
 
-    Returns:
-      None
     """
     df = read_file(filepath)
     print(df)
@@ -249,6 +190,25 @@ def main(filepath, destination, budget, important_factor="price"):
         main(filepath, destination, budget, important_factor)
     else:
         sys.exit()
+
+def parse_args(arglist):
+    """
+    Parses the command-line arguments.
+    
+    Args:
+      arglist (list): List of command-line arguments.
+      
+    Returns:
+
+    """
+    parser = ArgumentParser()
+    parser.add_argument("filepath", help="path to CSV File")
+    parser.add_argument("destination", help="name of destination")
+    parser.add_argument("budget", type=int, help="budget for trip")
+    parser.add_argument("-i", "--important", default="price",
+                        help="originally set to price. User may set this to price, food or convenience.")
+    args = parser.parse_args(arglist)
+    return args
 
 if __name__ == "__main__":
     args = parse_args(sys.argv[1:])
